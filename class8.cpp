1ed7efbb81703850d241e53ff4ceb4c2715dde94
Aditta Kirttania <147459210+adittak@users.noreply.github.com>
2024-01-15 09:55:26 -0500

    class8.cpp
    run time, compile time, 
    using new, delete

---
+++ class8.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Test
+{
+    int n;
+public:
+    Test(int val)
+    {
+        n = val;
+        cout << n;
+    }
+};
+
+int main()
+{
+    int a = 10; // compile time -> stack        //declaring nomral int without new would make it save in the stack form
+    string b ="hello";
+
+    int arr[3];
+    arr[0]=4;
+    arr[1]=5;
+    arr[2]=6;
+
+    int* ptr = new int(10); // run time -> heap     //using new would save the memery in heap 
+   // cout << *ptr << endl;
+    string* p = new string("hi");
+    //cout << *p;
+
+    //Test t(5); // compile time
+    Test* p2 = new Test(10); // run time
+    delete p2;
+
+    return 0;
+}
